5e47747d3c449fe4dd2dd2f9a24d8e7bb6f12d5c
quzhi323 <quzhi323@gmail.com>
2017-08-18 10:58:42 -0400

    Done
    Signed-off-by: quzhi323 <quzhi323@gmail.com>

--- update/update.py
+++ update/update.py
@@ -7,6 +7,12 @@ from utility.ReadFD import reFD
 from utility.ReadJson import reJson
 
 def findFdPatterns(df,fdcolumns):
+    '''
+    Find the Value Patterns according to FD
+    :param df: dataframe
+    :param fdcolumns: list of all fd columns, [3,4,5]
+    :return: pattern list
+    '''
     patternlist=[]
     length=len(df)
     for l in range(0,length):
@@ -18,6 +24,9 @@ def findFdPatterns(df,fdcolumns):
     return patternlist
 
 class UpdateValue():
+    '''
+    Obj UpdateValue represents the basic tupple in the dataframe.
+    '''
     def __init__(self,row,column,value):
         self.row=row
         self.column=column,
@@ -42,6 +51,9 @@ class UpdateValue():
         return self.value
 
 class UpdateFD():
+    '''
+    Obj UpdateFD represents the update for FD columns, including several UpdateValues according to the FD columns
+    '''
     def __init__(self,UpdateValueList):
         self.updates=UpdateValueList
 
@@ -52,13 +64,23 @@ class UpdateFD():
         self.updates=UpdateValueList
 
 def createUpdate(amount,df,domain,fdcolumns,pattern):
+
     '''
-    :param amount:
-    :param df:
-    :param domain:
-    :param fdcolumns:
-    :param pattern:
-    :return:
+    Create UpdateValue or UpdateFD randomly
+    :param amount: an argument controling the number of updates
+    :param df: dataframe
+    :param domain: 1.saving the information of NonFD-columns' value domain, there are three kinds of domain: number,value,ontology
+                   domain=[{0:{'number':'1,1000'}},
+                         {1:{'value':gen}},
+                         {2:{'ontology':age}}]
+                   2.for value domain, you have to have a pre definded value dict like,
+                   gen={1:'male',2:'female'}
+                   3.for ontology domain, you have to have a pre definded root like,
+                   age=reJson('../data/test1/age.json')
+                   function reJson() can resolve the ontology file in the format of json
+    :param fdcolumns: list of all fd columns, [3,4,5]
+    :param pattern: FD patterns, got from function findFdPatterns()
+    :return: a list of all Updates
     '''
     updatelist=[]
     for i in range(0,amount):
@@ -106,71 +128,10 @@ def createUpdate(amount,df,domain,fdcolumns,pattern):
             updatelist.append(updfd)
 
     return updatelist
-# def createUpdatesCoverEveryRow(amount,df,domain,fdcolumns,pattern):
-#
-#     updates=[]
-#
-#     for i in range(0,amount):
-#         for randomrow in range(0,len(df)-1):
-#             tag=random.randint(0,1)
-#             if tag==0:  ##更改非fd列
-#                 update = []
-#                 max=len(domain)-1
-#                 rand=random.randint(0,max)
-#                 randomcol=domain[rand]
-#
-#                 for c in randomcol:
-#                     col=c
-#                     coldom=randomcol[c]
-#
-#                 for d in coldom:
-#                     if d=='ontology':
-#                         root=coldom[d]
-#                         oldvalue=str(df.ix[randomrow][col])
-#                         choice=getRandom(root, oldvalue)
-#                         record=[randomrow,col,choice]
-#                         update.append(record)
-#
-#                     if d=='value':
-#                         dd=coldom[d]
-#                         max=0
-#                         for i in dd:
-#                             max=max+1
-#                         choice=dd[random.randint(1,2)]
-#                         record=[randomrow,col,choice]
-#                         update.append(record)
-#
-#                     if d=='number':
-#                         dd = coldom[d]
-#                         rg=dd.split(',')
-#                         min=int(rg[0])
-#                         max=int(rg[1])
-#                         choice=random.randint(min,max)
-#                         record=[randomrow,col,choice]
-#                         update.append(record)
-#
-#                 updates.append(update)
-#
-#             if tag == 1:
-#                 update = []
-#                 pat = random.sample(pattern, 1)[0]
-#                 l=len(pat)
-#                 for t in range(0, len(pat)):
-#                     record = [randomrow, fdcolumns[t], pat[t]]
-#                     # print(record)
-#                     update.append(record)
-#
-#                 updates.append(update)
-#
-#     return updates
+
 def createUpdatesCoverEveryRow(df,domain,fdcolumns,pattern):
     '''
-    :param amount:
-    :param df:
-    :param domain:
-    :param fdcolumns:
-    :param pattern:
-    :return:
+    A specific method to create updates covering every row, based on createUpdate()
     '''
     updatelist=[]
     for i in range(0,len(df)-1):
@@ -217,10 +178,12 @@ def createUpdatesCoverEveryRow(df,domain,fdcolumns,pattern):
             updfd = UpdateFD(updatevaluelist)
 
             updatelist.append(updfd)
-
     return updatelist
 
 def createUpdates(amount,df,domain,fdcolumns,pattern):
+    '''
+    A specific method to create updates at least cover every row, based on createUpdate() and createUpdatesCoverEveryRow()
+    '''
     updates=createUpdatesCoverEveryRow(df,domain,fdcolumns,pattern)
     max=random.randint(100,1000)
     for t in range(0,max):
@@ -228,6 +191,13 @@ def createUpdates(amount,df,domain,fdcolumns,pattern):
     return updates
 
 def select(root,value,exp_level):
+    '''
+    According to the 3 arguments, we can find the specific Value in OntologyTree
+    :param root:
+    :param value:
+    :param exp_level:
+    :return:
+    '''
     node=findNodes(root,value)
     if node is not None:
         level=node.getHeight()
@@ -254,6 +224,14 @@ def select(root,value,exp_level):
             return rsl
 
 def query(qry,exp_level,rootdic):
+    '''
+    Using pandasql to do query job. Before the final result, the output of query have to call select()
+    to change to one expected value
+    :param qry:
+    :param exp_level:
+    :param rootdic:
+    :return:
+    '''
     from pandasql import sqldf
     pysqldf = lambda q: sqldf(q, globals())
     rsl=pysqldf(qry)
@@ -268,7 +246,12 @@ def query(qry,exp_level,rootdic):
     return gersl
 
 def apply(df,Update):
-
+    '''
+    Apply update on DF
+    :param df:
+    :param Update:
+    :return:
+    '''
     master=[]
     update=Update
     if isinstance(update,UpdateValue) is True:
@@ -293,7 +276,17 @@ def apply(df,Update):
     return df,master
 
 def filter(df,updates,qry,exp_level,rootdic):
-
+    '''
+    Using specific query to check those updates applied on the dataframe.
+    If the result of query is the same between the original dataframe and the dataframe with update, then the update can be
+    append to the return result list.
+    :param df:
+    :param updates:
+    :param qry:
+    :param exp_level:
+    :param rootdic:
+    :return:
+    '''
     rsl=[]
     correct=query(qry,exp_level,rootdic)
 
@@ -315,6 +308,7 @@ def filter(df,updates,qry,exp_level,rootdic):
 
     return rsl
 
+
 path='../data/test1/1000gdb.csv'
 df=pd.read_csv(path,header=0)
 df=pd.DataFrame(df,columns=['PID','GEN','AGE','SYMP','DRUG','ILLNESS'])
@@ -329,11 +323,9 @@ domain=[{0:{'number':'1,1000'}},
         {1:{'value':gen}},
         {2:{'ontology':age}},
        ]
-
-
 pattern=findFdPatterns(df,fdcolumns)
 updates=createUpdates(1,df,domain,fdcolumns,pattern)
-
+rootdic={'AGE':age,'SYMP':symp}
 # print('---------TEST UPDATE----------')
 # for z in updates:
 #      print('=============================')
@@ -365,7 +357,6 @@ updates=createUpdates(1,df,domain,fdcolumns,pattern)
 # print('---------TEST QUERY----------')
 qry="select SYMP from df where GEN='female' ;"
 exp_level=2
-rootdic={'AGE':age,'SYMP':symp}
 # print(query(qry,exp_level,rootdic))
 # print('*******')
 # print('')
